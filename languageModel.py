2fab15d894fa6bf2beec86131254b350aaa117bc
Laurent-Zhu <2145394902@qq.com>
2025-06-29 12:49:18 +0800

    完成了第8章 循环神经网络 的学习

---
+++ languageModel.py
@@ -0,0 +1,11 @@
+import random
+import torch
+from d2l import torch as d2l
+
+# 语言模型
+
+tokens = d2l.tokenize(d2l.read_time_machine())
+corpus = [token for line in tokens for token in line]
+vocab = d2l.Vocab(corpus)
+print(vocab.token_freqs[:10])
+
